96b5909b787b33f30d802f2860e867c8c55c5fb1
Rajesh G <rajesh@unigps.in>
2021-02-24 02:33:30 +0530

    base code

---
+++ src/main/java/in/brainupgrade/weather/WeatherController.java
@@ -0,0 +1,29 @@
+package in.brainupgrade.weather;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import in.brainupgrade.weather.model.currentWeather.City;
+import in.brainupgrade.weather.model.currentWeather.WeatherForecast;
+
+@Controller
+public class WeatherController {
+
+	@Autowired
+	private RemoteApiFetcher remoteApiFetcher;
+
+	@RequestMapping(value = "/get-weather", method = { RequestMethod.PUT, RequestMethod.POST })
+	public Optional<WeatherForecast> fetchWeatherForecastFromRemoteApi(City city) {
+		return remoteApiFetcher.fetchWeatherForecastFromRemoteApi(city);
+	}
+
+	@RequestMapping(value = "/get-cities", method = { RequestMethod.PUT, RequestMethod.POST })
+	public Optional<City[]> fetchCitiesFromRemoteApi(String cityInput) {
+		return remoteApiFetcher.fetchCitiesFromRemoteApi(cityInput);
+	}
+
+}
